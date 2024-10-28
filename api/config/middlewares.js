b7a356be2e24dd7e68ef8afe1a11574561d08bc1
soniikot <soniikot@gmail.com>
2024-10-28 15:23:03 -0700

    add cors

--- api/config/middlewares.js
+++ api/config/middlewares.js
@@ -2,7 +2,17 @@ module.exports = [
   "strapi::logger",
   "strapi::errors",
   "strapi::security",
-  "strapi::cors",
+
+  // Other middlewares...
+  {
+    name: "strapi::cors",
+    config: {
+      origin: ["https://62.72.5.244:5173/"],
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+    },
+  },
+
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
