4f54014cb24af8068b5f2bc9f6c8d02079aab6b7
soniikot <soniikot@gmail.com>
2024-10-28 15:43:55 -0700

    fix middleware

--- api/config/middlewares.js
+++ api/config/middlewares.js
@@ -7,7 +7,7 @@ module.exports = [
   {
     name: "strapi::cors",
     config: {
-      origin: ["https://62.72.5.244:5173/"],
+      origin: ["https://62.72.5.244:5173"],
       methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
       allowedHeaders: ["Content-Type", "Authorization"],
     },
