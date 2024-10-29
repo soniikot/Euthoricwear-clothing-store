0157548a195a5fe8f9b50a20c2cf29080563a014
soniikot <soniikot@gmail.com>
2024-10-29 09:06:09 -0700

    change header

--- api/config/middlewares.js
+++ api/config/middlewares.js
@@ -2,16 +2,16 @@ module.exports = [
   "strapi::logger",
   "strapi::errors",
   "strapi::security",
-
   "strapi::poweredBy",
 
   {
     name: "strapi::cors",
     config: {
       origin: [
-        "http://62.72.5.244:5173, http://zkg4g80ok4ss00ok8o4kwo84.62.72.5.244.sslip.io",
+        "http://62.72.5.244:5173",
+        "http://zkg4g80ok4ss00ok8o4kwo84.62.72.5.244.sslip.io",
       ],
-      headers: ["Authorization", "Content-Type", "Accept"],
+      headers: ["authorization", "content-Type", "accept"],
       methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     },
   },
