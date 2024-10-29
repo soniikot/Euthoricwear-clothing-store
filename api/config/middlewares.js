65136d291573bda5ac1099d27001834c1dda8172
soniikot <soniikot@gmail.com>
2024-10-29 08:41:36 -0700

    middleware

--- api/config/middlewares.js
+++ api/config/middlewares.js
@@ -8,13 +8,11 @@ module.exports = [
   {
     name: "strapi::cors",
     config: {
-      enabled: true,
       origin: [
         "http://62.72.5.244:5173, http://zkg4g80ok4ss00ok8o4kwo84.62.72.5.244.sslip.io",
       ],
       headers: ["Authorization", "Content-Type", "Accept"],
       methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-      keepHeaderOnError: true,
     },
   },
 
