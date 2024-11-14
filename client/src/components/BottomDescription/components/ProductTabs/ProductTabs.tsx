e24b249d49311ca5a3ba3c9b3876ce1c61d83b5b
soniikot <soniikot@gmail.com>
2024-11-14 14:36:07 -0800

    fix tabs

--- client/src/components/BottomDescription/components/ProductTabs/ProductTabs.tsx
+++ client/src/components/BottomDescription/components/ProductTabs/ProductTabs.tsx
@@ -1,63 +1,45 @@
 import { FC, useState } from 'react';
-import { type SyntheticEvent } from 'react';
 import style from './styles.module.scss';
-import { Box } from '@mui/material';
-import { TabContext, TabList } from '@mui/lab';
+import { Tab, Box } from '@mui/material';
+import { TabPanel, TabContext, TabList } from '@mui/lab';
 import product_table from '@assets/product_table.png';
-import { CustomTab } from './components/CustomTab/CustomTab';
-import { CustomTabPanel } from './components/CustomTabPanel/CustomTabPanel';
 
 export const ProductTabs: FC = () => {
   const [value, setValue] = useState('1');
-  const handleTabChange = (_event: SyntheticEvent, newValue: string) => {
+
+  const handleTabChange = (event: MouseEvent, newValue: string) => {
     setValue(newValue);
   };
-
   return (
     <>
       <div className={style.wrapper}></div>
       <TabContext value={value}>
-        <Box
-          sx={{
-            borderBottom: 1,
-
-            fontSize: {
-              xs: '10px',
-              sm: '16px',
-            },
-          }}
-        >
+        <Box sx={{ borderBottom: 1 }}>
           <TabList
             onChange={handleTabChange}
             textColor="secondary"
             indicatorColor="secondary"
           >
-            <CustomTab label="Description" value="1" />
-            <CustomTab label="User comments" value="2" />
-            <CustomTab label="Question & Answer" value="3" />
+            <Tab label="Description" value="1" />
+            <Tab label="User comments" value="2" />
+            <Tab label="Question & Answer" value="3" />
           </TabList>
         </Box>
-        <CustomTabPanel
-          value="1"
-          content={
-            <>
-              <p>
-                100% Bio-washed Cotton - makes the fabric extra soft & silky.
-                Flexible ribbed crew neck. Precisely stitched with no pilling &
-                no fading. Provide all-time comfort. Anytime, anywhere. Infinite
-                range of matte-finish HD prints.
-              </p>
-              <br />
-              <img
-                className={style.product_table}
-                src={product_table}
-                alt="product_description"
-              />
-            </>
-          }
-        />
-        <CustomTabPanel value="2" content=" This is nice product" />
-        <CustomTabPanel value="3" content=" You can wash it with everything" />
+        <TabPanel value="1" sx={{ width: 610, height: 350, overflowY: 'auto' }}>
+          100% Bio-washed Cotton - makes the fabric extra soft & silky. Flexible
+          ribbed crew neck. Precisely stitched with no pilling & no fading.
+          Provide all-time comfort. Anytime, anywhere. Infinite range of
+          matte-finish HD prints.
+          <br />
+          <br />
+          <img src={product_table} alt="product_description" />
+        </TabPanel>
+        <TabPanel value="2" sx={{ width: 610, height: 350, overflowY: 'auto' }}>
+          This is nice product
+        </TabPanel>
+        <TabPanel value="3" sx={{ width: 610, height: 350, overflowY: 'auto' }}>
+          You can wash it with everything
+        </TabPanel>
       </TabContext>
     </>
   );
