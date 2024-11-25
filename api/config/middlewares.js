1097d4042345f226ef990b9b9d17b7787ae537f0
soniikot <soniikot@gmail.com>
2024-11-24 23:42:18 -0800

    database update

--- api/config/middlewares.js
+++ api/config/middlewares.js
@@ -10,10 +10,9 @@ module.exports = [
       origin: [
         "http://62.72.5.244:5173",
         "http://62.72.5.244:8000",
-        "http://localhost:5173",
         "http://zkg4g80ok4ss00ok8o4kwo84.62.72.5.244.sslip.io",
       ],
-      headers: ["authorization", "content-Type", "accept"],
+      headers: ["authorization", "Content-Type", "accept"],
       methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
       credentials: true,
     },
