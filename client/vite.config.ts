9311befd52c8bf4de239e8af3ee2bce8bf39962e
soniikot <soniikot@gmail.com>
2024-12-14 10:31:19 -0800

    delete proxy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,14 +7,6 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-    proxy: {
-      '/api': {
-        target: 'https://euphoricwear.shop',
-        changeOrigin: true,
-        secure: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
   },
   resolve: {
     alias: {
