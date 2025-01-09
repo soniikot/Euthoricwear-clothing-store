6b6b2abd172e96f793808013fb90789d431ca555
soniikot <soniikot@gmail.com>
2025-01-09 12:14:55 -0800

    add data attributes for tests

--- client/src/tests/Cart.test.tsx
+++ client/src/tests/Cart.test.tsx
@@ -6,6 +6,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from '../features/cart/cartSlice';
 import userReducer from '../features/user/userSlice';
 import testImage from '../assets/sample1.jpg';
+
 // Test store setup
 const createTestStore = (preloadedState = {}) => {
   return configureStore({
@@ -113,39 +114,9 @@ describe('Cart Component', () => {
         cart: { cart: cartItems },
         user: { username: '' },
       });
-      const productRow = screen
-        .getByText('Second Product')
-        ?.closest('.container');
-      if (!productRow) {
-        throw new Error('Product row not found');
-      }
-      const quantityContainer = productRow.querySelector(
-        '._quantity_204c46 > ._wrapper_5508f4'
-      );
-      if (!quantityContainer) {
-        throw new Error('Quantity container not found');
-      }
-      if (!quantityContainer) {
-        throw new Error('Quantity container not found');
-      }
-
-      const quantityDisplay =
-        quantityContainer.querySelector('div:nth-child(2)');
-      const incrementButton =
-        quantityContainer.querySelector('button:last-child');
-      const decrementButton =
-        quantityContainer.querySelector('button:first-child');
-
-      if (!quantityDisplay) {
-        throw new Error('Quantity display not found');
-      }
-      if (!incrementButton) {
-        throw new Error('Increment button not found');
-      }
-      if (!decrementButton) {
-        throw new Error('Decrement button not found');
-      }
-
+      const decrementButton = screen.getByTestId('decrement-button-2');
+      const incrementButton = screen.getByTestId('increment-button-2');
+      const quantityDisplay = screen.getByTestId('count-display-2');
       expect(quantityDisplay.textContent).toBe('2');
 
       fireEvent.click(incrementButton);
