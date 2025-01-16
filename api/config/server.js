dc0f0e769b998546bf11b340489411b93e49dc56
soniikot <soniikot@gmail.com>
2025-01-15 22:16:56 -0800

    update cors

--- api/config/server.js
+++ api/config/server.js
@@ -3,7 +3,6 @@ module.exports = ({ env }) => ({
   port: env.int("PORT", 1337),
   app: {
     keys: env.array("APP_KEYS"),
-    url: "https://euphoricwear.online/",
   },
   webhooks: {
     populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
