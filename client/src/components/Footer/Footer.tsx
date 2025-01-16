3d030a3f3eaadf218b7951ddd184383da1f0c7da
soniikot <soniikot@gmail.com>
2025-01-16 14:46:16 -0800

    change social media icons to svg

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -2,8 +2,8 @@ import style from './styles.module.scss';
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
 import facebook from '@/assets/facebook.svg';
-import twitter from '@/assets/twitter.png';
-import instagram from '@/assets/instagram.png';
+import twitter from '@/assets/twitter.svg';
+import instagram from '@/assets/instagram.svg';
 import link from '@/assets/link.svg';
 
 export const Footer: FC = () => {
