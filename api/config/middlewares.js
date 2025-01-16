d0330a9ee28afac8a4abc4a214372205a2981e72
soniikot <soniikot@gmail.com>
2025-01-15 22:39:15 -0800

    update cors

--- api/config/middlewares.js
+++ api/config/middlewares.js
@@ -11,7 +11,7 @@ module.exports = [
         "http://localhost:5173",
         "http://62.72.5.244:5173",
         "http://62.72.5.244:8000",
-        "https://euphoricwear.shop/",
+        "https://euphoricwear.shop",
       ],
       headers: ["authorization", "content-type", "accept"],
       methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
