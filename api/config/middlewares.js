c912350c057f787d85670fd7366098c72d72635e
soniikot <soniikot@gmail.com>
2025-01-15 16:19:04 -0800

    update CORS

--- api/config/middlewares.js
+++ api/config/middlewares.js
@@ -12,6 +12,7 @@ module.exports = [
         "http://62.72.5.244:5173",
         "http://62.72.5.244:8000",
         "http://zkg4g80ok4ss00ok8o4kwo84.62.72.5.244.sslip.io",
+        "https://api.euphoricwear.shop/",
       ],
       headers: ["authorization", "content-type", "accept"],
       methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
