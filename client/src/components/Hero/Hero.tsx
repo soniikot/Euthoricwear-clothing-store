c9e2ec10f9d51b403c22a4db0e362edd357a6805
soniikot <soniikot@gmail.com>
2025-01-16 12:53:24 -0800

    fix mobile hero

--- client/src/components/Hero/Hero.tsx
+++ client/src/components/Hero/Hero.tsx
@@ -4,20 +4,18 @@ import { FC } from 'react';
 
 export const Hero: FC = () => {
   return (
-    <div className={style.background}>
-      <div className="container">
-        <div className={style.wrapper}>
-          <div className={style.text}>
-            <h3>T-Shirts / Tops</h3>
-            <h1 className={style.title}>Summer Value Pack</h1>
-            <h3>cool / colorful / comfy</h3>
-            <div className={style.button}>
-              <TextButtonWithLink
-                text="Shop Now"
-                buttonColor="white"
-                link="/products/"
-              />
-            </div>
+    <div className="container">
+      <div className={style.wrapper}>
+        <div className={style.text}>
+          <h3>T-Shirts / Tops</h3>
+          <h1 className={style.title}>Summer Value Pack</h1>
+          <h3>cool / colorful / comfy</h3>
+          <div className={style.button}>
+            <TextButtonWithLink
+              text="Shop Now"
+              buttonColor="white"
+              link="/products/"
+            />
           </div>
         </div>
       </div>
